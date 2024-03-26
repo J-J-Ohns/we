56b59571b7816d4548fbce72ba6f97e603187000
J-J-Ohns <mr.j.m.lass@googlemail.com>
2024-03-26 16:26:37 +0100

    incl uebung 30

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', (req, res, next) => {
     res.render('index', { title: 'Express' })
 })
 
+router.get('/addList', function (req, res, next) {
+    res.render('addList')
+})
+
 module.exports = router
