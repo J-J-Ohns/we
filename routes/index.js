a72661f03cd8b2c307cbaf2d4835479e06878332
J-J-Ohns <mr.j.m.lass@googlemail.com>
2024-03-26 15:49:55 +0100

    fuenfpunktvierzehnfeddisch

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express', words: ['EJS', 'is', '<span style="font-weight: bold">awesome</span>']})
-})
+// router.get('/', function (req, res, next) {
+    // res.render('index', { title: 'Express', words: ['EJS', 'is', '<span style="font-weight: bold">awesome</span>']})
+// })
 
-router.get('/addList', function (req, res, next) {
-    res.render('addList')
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Express',content: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    })
 })
 
 module.exports = router
